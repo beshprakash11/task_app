03078be1b08b6d7c0097c9276d62a92aeb5cbbe8
beshprakash11 <beshprakashktm@gmail.com>
2023-02-18 10:30:46 +0100

    main page updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(     
+      title: 'Task App',
       home: Text("Hello"),
     );
   }
