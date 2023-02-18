b23a399511ae727c414de2ee17fea6fe10ecb7f2
beshprakash11 <beshprakashktm@gmail.com>
2023-02-18 12:07:31 +0100

    Task model access from tasks list widget

--- lib/screens/home/widgets/tasks.dart
+++ lib/screens/home/widgets/tasks.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/models/task.dart';
 
 class Tasks extends StatelessWidget {
-  const Tasks({super.key});
+  final tasksList = Task.generateTask();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Task list'),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10
+        ), 
+        itemBuilder: itemBuilder
+      ),
     );
   }
+  
 }
