fb6089597edaa52fc113e260f545da3dac06da2d
beshprakash11 <beshprakashktm@gmail.com>
2023-02-18 10:51:46 +0100

    GoPremium pages added

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/screens/home/widgets/go_premium.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,36 +8,45 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Row(
-          children: [
-            Container(
-              height: 45,
-              width: 45,
-              margin: EdgeInsets.only(left: 15),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset('assets/images/avatar.jpg'),
-              ),
-            ),
-            const SizedBox(width: 10,),
-            const Text(
-              'Hi, BPA',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-          ],
-        ),
-        actions: [
-          Icon(Icons.more_vert, color: Colors.black, size: 40,)
+      appBar: _buildAppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GoPremium(),
         ],
       ),
-      body: Center(child: Text("First pages")),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Row(
+        children: [
+          Container(
+            height: 45,
+            width: 45,
+            margin: const EdgeInsets.only(left: 15),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset('assets/images/avatar.jpg'),
+            ),
+          ),
+          const SizedBox(width: 10,),
+          const Text(
+            'Hi, BPA',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 26,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        Icon(Icons.more_vert, color: Colors.black, size: 40,)
+      ],
     );
   }
 }
