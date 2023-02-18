ee0865373a27293993b05f924b533201f827703a
beshprakash11 <beshprakashktm@gmail.com>
2023-02-18 10:32:35 +0100

    Homepaged added and updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/screens/home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(     
       title: 'Task App',
-      home: Text("Hello"),
+      home: HomePage(),
     );
   }
 }
