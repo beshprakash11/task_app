999afb8961cafe2b479c47c27fcb854274b37d4a
beshprakash11 <beshprakashktm@gmail.com>
2023-02-18 10:35:15 +0100

    Main pages updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:task_app/screens/home/home.dart';
 
 void main() {
@@ -11,7 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(     
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    );
+    return MaterialApp(   
+      debugShowCheckedModeBanner: false,  
       title: 'Task App',
       home: HomePage(),
     );
