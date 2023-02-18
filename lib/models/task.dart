c9603202a5a779512de4c49a237db30207dc1b04
beshprakash11 <beshprakashktm@gmail.com>
2023-02-18 11:44:19 +0100

    Task model created with entity

---
+++ lib/models/task.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Task{
+  IconData? iconData;
+  String? title;
+  Color? bgColor;
+  Color? iconColor;
+  Color? btnColor;
+  num? left;
+  num? done;
+  bool? isLast;
+}
--- lib/screens/home/widgets/tasks.dart
+++ lib/screens/home/widgets/tasks.dart
@@ -5,6 +5,8 @@ class Tasks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('Task list'),
+    );
   }
 }
