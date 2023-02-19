9e8e6de924942368da1f292562d4932107147567
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 08:12:17 +0100

    Route to detail pages

--- lib/screens/home/widgets/tasks.dart
+++ lib/screens/home/widgets/tasks.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:task_app/models/task.dart';
 
 import 'package:dotted_border/dotted_border.dart';
+import 'package:task_app/screens/detail/detail.dart';
 
 class Tasks extends StatelessWidget {
   final tasksList = Task.generateTask();
@@ -46,7 +47,10 @@ class Tasks extends StatelessWidget {
   Widget _buildTask(BuildContext context, Task task){
     return GestureDetector(
       onTap: () {
-        
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => DetailPage(task))
+
+        );
       },
       child: Container(
         padding: EdgeInsets.all(15),
