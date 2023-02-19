24d02a09ed5a0deb96874b1d7f6770b851b4f5cf
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 08:42:33 +0100

    Datepicker pages updated to statefull widget

--- lib/screens/detail/widgets/date_picker.dart
+++ lib/screens/detail/widgets/date_picker.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class DatePicker extends StatelessWidget {
+class DatePicker extends StatefulWidget {
   const DatePicker({super.key});
 
+  @override
+  State<DatePicker> createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
   @override
   Widget build(BuildContext context) {
     return Container();
