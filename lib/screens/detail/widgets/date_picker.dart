baf2258a96baea968f7fa03b9ef68798b1d807ad
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 08:46:11 +0100

    Weak list with day added manually

--- lib/screens/detail/widgets/date_picker.dart
+++ lib/screens/detail/widgets/date_picker.dart
@@ -8,6 +8,11 @@ class DatePicker extends StatefulWidget {
 }
 
 class _DatePickerState extends State<DatePicker> {
+  final weakList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  final dayList = ['24','25', '26', '27', '28', '29', '30']; 
+
+  var selected = 4;
+
   @override
   Widget build(BuildContext context) {
     return Container();
