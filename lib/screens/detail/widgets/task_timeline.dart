7e3db855a989ccfd0044261ed5adecc76a59c7f0
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 10:59:50 +0100

    timeline updated

--- lib/screens/detail/widgets/task_timeline.dart
+++ lib/screens/detail/widgets/task_timeline.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 
 class TaskTimeline extends StatelessWidget {
   final Map<String, dynamic> detail;
@@ -18,6 +19,14 @@ class TaskTimeline extends StatelessWidget {
   }
 
   Widget _buildTimeline(Color color, ){
-    return Container();
+    return Container(
+      height: 80,
+      width: 20,
+      child: TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0,
+        isFirst: true,
+      ),
+    );
   }
 }
