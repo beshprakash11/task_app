9871d3adb511c39b81283eac0bd4c13f77ebcb0b
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 10:30:40 +0100

    Task time line pages added

---
+++ lib/screens/detail/widgets/task_timeline.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskTimeline extends StatelessWidget {
+  const TaskTimeline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
